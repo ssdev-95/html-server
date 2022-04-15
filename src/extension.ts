09438e064edbd8b250d98a0564267be09da52be8
Salomao S <souza95salomao@gmail.com>
2022-04-15 17:16:40 -0400

    [UPDATE] Add some functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,42 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+
 	console.log('Congratulations, your extension "html-server" is now active!');
 
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('html-server.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from html-server!');
-	});
+	//  Now provide the implementation of the command with registerCommand
+	//  The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand(
+		'html-server.helloWorld',
+		() => {
+	  	// Display a message box to the user
+		  vscode.window.showInformationMessage('Hello World from html-server!');
+		}
+	);
 
-	let lol = vscode.commands.registerCommand('Hi', () => {
-		const workspace = vscode.workspace.getWorkspaceFolder.name;
-		vscode.window.showWarningMessage(workspace);
-	});
+	let start = vscode.commands.registerCommand(
+		'html-server.start',
+		() => {
+		  const workspace = vscode.workspace.getWorkspaceFolder.name;
+			vscode.window.showInformationMessage(`Started server on folder ${workspace}!`);
+		}
+	);
+
+	let stop = vscode.commands.registerCommand(
+		'html-server.stop',
+		() => {
+			vscode.window.showInformationMessage('Server successfully stoped!')
+		}
+	)
 
 	context.subscriptions.push(disposable);
-	context.subscriptions.push(lol);
+	context.subscriptions.push(start);
+	context.subscriptions.push(stop);
 }
 
 // this method is called when your extension is deactivated
